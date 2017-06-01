dd567087809243c0bf080622749ba2e6af0b26be
Adrian Stabiszewski <as@grundid.de>
2017-06-01 20:37:47 +0200

    first version

---
+++ app.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const kdbush = require("kdbush");
+const geokdbush = require('geokdbush');
+
+
+const plzData = fs.readFileSync("PLZ.tab").toString("ascii");
+const lines = plzData.split("\n");
+
+const result = {};
+const pointToPlz = {};
+
+const points = [];
+const plzs = [];
+
+lines.forEach((line, index) => {
+    if (index > 0) {
+        const lineContent = line.split("\t");
+        const plz = lineContent[1];
+        const point = [lineContent[2], lineContent[3]];
+        if (plz !== undefined) {
+            points.push(point);
+            plzs.push(plz);
+            result[plz] = point;
+            const pointKey = point.join("#");
+            pointToPlz[pointKey] = plz;
+        }
+    }
+});
+fs.writeFileSync("plz.json", JSON.stringify(result, null, 1));
+
+const pointIndex = kdbush(points);
+
+const targetPlz = "74072";
+const targetPoint = result[targetPlz];
+
+console.log("targetPoint: ", targetPoint);
+var nearest = geokdbush.around(pointIndex, targetPoint[0], targetPoint[1], 1000, 5);
+
+nearest.forEach(function (point) {
+    const key = point.join("#");
+    const plz = pointToPlz[key];
+    console.log(plz);
+});
+
+
+
+
+
+
+
+
+
